278d178dc0b821c72fd61da656f977e827f5e1c8
Mohaddisa Zehra <k250663@nu.edu.pk>
2025-11-26 09:55:11 +0500

    Update final.c

--- final.c
+++ final.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <time.h>
+#include <time.h>         //for timer
 #include<string.h>
-#include<stdlib.h>
+#include<stdlib.h>                  //for clearing screen
+#include<unistd.h>             //for usleep - transition
 void rules();
 int levels();
 int level;
 int type_test(const char *sentence);
 void save_score(const char *name , float score);
 float score = 0;
+
 
 
 void print_colored_sentence(const char *sentence, int level) {
@@ -25,15 +27,23 @@ void print_colored_sentence(const char *sentence, int level) {
 }
 
 void save_score(const char *name , float score){
+	char *lvl_ptr;
     FILE *file = fopen("scores.txt" , "a");   //a is used for append
-    fprintf(file , "\n\nNAME :%s" , name);
-    fprintf(file , "level(1.easy , 2.hard):%d\n" , level);
-    fprintf(file , "SCORE :%.2f/5\n" , score);
-    
      if (file==NULL) {
         printf("Error\n");
         return;
     }
+    fprintf(file , "\n\nNAME :%s\n" , name);
+    if(level == 1){
+    	lvl_ptr = "easy";
+	}
+	else{
+		lvl_ptr = "hard";
+	}
+    fprintf(file , "Level : %s\n" , lvl_ptr);
+    fprintf(file , "SCORE :%.2f/5\n" , score);
+    
+
     fclose(file);
     printf("\nscores saved!!");
 }
@@ -43,18 +53,29 @@ void rules(){
 	printf("1. Your goal is to type the displayed sentence exactly as shown.\n2. Capitalization, spaces, and punctuation must be correct.\n3. The timer starts as soon as the sentence is displayed.\n4. Press 'Enter' after completing the sentence.\n5. Accuracy must be 99% or higher to earn a point.\n6. Word Per Second (WPS) and accuracy will be shown after each sentence.\n7. You will be given 5 sentences to type.\n8. Your final score will be out of 5.\n9. Do not use backspace while typing (if possible).\n10. Try to complete the sentence as fast and accurately as possible.\nGOOD LUCK!");
 }
 int levels(){
-	printf("choose between\n\033[33m1.EASY MODE\n\033[36m2.HARD MODE\033[0m\n");
+	while(1){
+	
+	printf("Choose between\n\033[33m1.EASY MODE\n\033[36m2.HARD MODE\033[0m\n");
 	scanf("%d" , &level);
-	return level;
+	if(level == 1|| level==2){
+
+		return level;
+}
+	else{
+		printf("invalid input");
+		
+}
+}
 }
 	
 
 int type_test(const char *sentence){
 		char uw[300] ;
+		memset(uw , 0 ,sizeof(uw));
 		
 		int ch;
 while((ch = getchar()) != '\n' && ch != EOF);           //when enter is pressed it clears the buffer including \n
-		printf("\n\ntype the following sentence:\n");
+		printf("\n\nType the following sentence:\n");
 		print_colored_sentence(sentence , level);
 		time_t start, end;
         time(&start);       //to record the start time 
@@ -66,7 +87,7 @@ while((ch = getchar()) != '\n' && ch != EOF);           //when enter is pressed 
 			double time_taken = difftime(end, start);   //to calc time taken
 				int len = strlen(sentence);
 			
-			float wps = len/time_taken;
+			float cps = len/time_taken;
 int correct = 0;
 for (int i =0 ; i<len ; i++){
 	if(sentence[i] == uw[i]){
@@ -74,7 +95,7 @@ for (int i =0 ; i<len ; i++){
 	}
 }
 for(int i =0; i <strlen(sentence); i++){
-    if(sentence[i] != uw[i]){
+    if(i>=strlen(uw)||(sentence[i] != uw[i])){
         printf("^"); // mistake
     } 
 	else {
@@ -85,7 +106,7 @@ double acc = ((double)correct/len)*100;
 	
 
 		printf("\naccuracy is:%.2f %%", acc);
-		printf("\nword per second count is :%.2f" , wps);     //words in one second
+		printf("\ncharacter per second count is :%.2f" , cps);     //char in one second
 		if(acc > 99){
 			score+=1;
 		}
@@ -95,6 +116,18 @@ double acc = ((double)correct/len)*100;
 		printf("\nscore:%.2f" , score);
 		return score;
 }
+void title_animation() {
+	printf("======================================================================================================================\n");
+    char title[] = "                              WELCOME TO TYPING TUTOR\n\n by 25k-0663 & 25k-0803";
+    printf("\n\033[32m");
+    for (int i = 0; i < strlen(title); i++) {
+        printf("%c", title[i]);
+        fflush(stdout);
+        usleep(50000);
+    }
+    printf("\033[0m\n");
+    printf("========================================================================================================================");
+}
 
 int main() {
 	while(1){
@@ -103,22 +136,27 @@ int main() {
 		#else
 			system("clear");            // for mac
 		#endif
+		
+		title_animation(); 
+		
 	int menu;
 	char name[50];
-  printf("\033[32m======================================= WELCOME TO TYPING TUTOR ======================================================\033[0m \n\n");
-  printf("---------MAIN MENU------------\n");
+  //printf("\033[32m======================================= WELCOME TO TYPING TUTOR ======================================================\033[0m \n\n");
+  printf("\n\n-MAIN MENU-\n");
   printf("1.START \n2.VIEW RULES\n");
   scanf("%d" , &menu);
   getchar();     //used to clear out previous 
   switch(menu){
   	case 1:
   		
-  	printf("enter your name: ");
+  	printf("Enter your name: ");
   fgets(name, sizeof(name), stdin);
   score = 0;
-  	levels();
+  level = levels();
+  char p;
   	if(level == 1){
-  			printf("\neasy mode selected!");
+
+  			printf("\nEasy mode selected!");
   		type_test("Humans have 206 bones");
   		type_test("Your heart pumps blood through your body");
   		type_test("Earth revolves around sun");
@@ -126,6 +164,11 @@ int main() {
   		type_test("Accuracy is more important than speed");
 	  }
 	  else{
+	printf("\a\a");       //beep
+	printf("Hard mode selected\n");
+	printf("\033[31mThis level is extra challenging are you sure you want to continue?(y/n): \033[0m");
+	scanf(" %c" , &p);
+	if(p =='y' || p =='Y'){
 	     type_test("#codelikeApro@$");
 	      type_test("That will be $20,847,817.92 (*** plus 5% tax)!");
         type_test("Fact #417: 2^8 is 2^(2^2)*2^(2^2))!");
@@ -133,7 +176,13 @@ int main() {
             type_test("The Milky Way contains over 100400 billion stars and moves at ~220 km/s around its center.");
             
 		  }
-		  printf("\n\nNAME: %sfinal score:%d/5\n" , name , score);
+		  else{
+		  	printf("Dont! Give up");
+		  	level = levels();
+		  }
+	}
+		  name[strcspn(name,"\n")]=0;
+		  printf("\n\nNAME: %s  final score: %.2f/5\n" , name , score);
   if(score == 5){
   	printf("well done");
   }
@@ -145,10 +194,12 @@ int main() {
   }
   
   save_score(name , score);
+    	printf("\npress enter to return back to the menu!");
+    	getchar();
   		break;	
   	case 2:
   	rules();
-  	printf("\nenter space to return back to the menu!");
+  	printf("\npress enter to return back to the menu!");
   	getchar();
   		
   	break;
@@ -159,5 +210,4 @@ int main() {
   }
     return 0;
 }
-
 
